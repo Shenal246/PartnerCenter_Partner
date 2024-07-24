8ed64a62c1004c0c9397c11d6bb41faacd5c924b
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-07-24 16:48:53 +0530

    commit

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -12,83 +12,106 @@ function HomePage() {
             <VerticalNavbar activeLinkId="home" />
             <div className="container">
 
-                <div className='row'>
-                    <div className='col-2'></div>
-                    <div className='col-10'>
-                        <div class="card UserCardBody mb-3" style={{ width: '90vh' }}>
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                    <img src={crdImg} class="img-fluid rounded-start" alt="..." />
+                {/* First row  */}
+                <div className='row '>
+                    <div className="cards-container rowFirst">
+                        <div className="card HomeCard1">
+                            <div className="card-content">
+                                <h2>Welcome to Connex Partner Portal</h2>
+                                <h3>Enhance Your Business with Connex</h3>
+                                <p>
+                                    Access powerful tools, resources, and support designed to help you succeed.
+                                    Join our community of partners today and unlock new opportunities.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="card HomeCard1">
+                            <div className="card-content profile-card">
+                                <div className='col-5'>
+                                    <img src={crdImg} alt="Profile" className="profile-img"  />
                                 </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">King DUTUGAMUNU</h5>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+
+                        
+                                <div class="col-md-7">
+                                    <div class="card-body HomeCardBody">
+                                        <div className="profile-info">
+                                            <p><strong>A.D C Sirisena</strong></p>
+                                            <p>Admin - CAX Solutions</p>
+                                            <p>ID-xxxxxxx</p>
+                                            <button className="profile-button">Profile</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
+
+
+
+
+
+                {/* second row  */}
+
                 <div className='row'>
                     <div className='col-2'></div>
                     <div className='col-8'>
-                    <div className='swiperContainer mt-4'>
-                        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <div className='swiperContainer mt-4'>
+                            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-indicators">
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                </div>
+                                <div className="carousel-inner">
+
+                                    <div className="carousel-item active" data-bs-interval="2000">
+                                        <div className='singleSlider'>
+                                            <img src={Imagesd} className="d-block w-100 sliderImage" alt="..." />
+                                            <div className='sliderButtonContainer'>
+                                                <button className='btn btn-info'>Check Product</button>
+                                                <button className='btn btn-info'>Request</button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="carousel-item" data-bs-interval="2000">
+                                        <div className='singleSlider'>
+                                            <img src={Imagesd} className="d-block w-100 sliderImage" alt="..." />
+                                            <div className='sliderButtonContainer'>
+                                                <button className='btn btn-info'>Check Product</button>
+                                                <button className='btn btn-info'>Request</button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="carousel-item" data-bs-interval="2000">
+                                        <div className='singleSlider'>
+                                            <img src={Imagesd} class="d-block w-100 sliderImage" alt="..." />
+                                            <div className='sliderButtonContainer'>
+                                                <button className='btn btn-info'>Check Product</button>
+                                                <button className='btn btn-info'>Request</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Previous</span>
+                                </button>
+                                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Next</span>
+                                </button>
                             </div>
-                            <div className="carousel-inner">
 
-                                <div className="carousel-item active" data-bs-interval="2000">
-                                    <div className='singleSlider'>
-                                        <img src={Imagesd} className="d-block w-100 sliderImage" alt="..." />
-                                        <div className='sliderButtonContainer'>
-                                            <button className='btn btn-info'>Check Product</button>
-                                            <button className='btn btn-info'>Request</button>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="carousel-item" data-bs-interval="2000">
-                                    <div className='singleSlider'>
-                                        <img src={Imagesd} className="d-block w-100 sliderImage" alt="..." />
-                                        <div className='sliderButtonContainer'>
-                                            <button className='btn btn-info'>Check Product</button>
-                                            <button className='btn btn-info'>Request</button>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="carousel-item" data-bs-interval="2000">
-                                    <div className='singleSlider'>
-                                        <img src={Imagesd} class="d-block w-100 sliderImage" alt="..." />
-                                        <div className='sliderButtonContainer'>
-                                            <button className='btn btn-info'>Check Product</button>
-                                            <button className='btn btn-info'>Request</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Previous</span>
-                            </button>
-                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Next</span>
-                            </button>
                         </div>
-
-                    </div>
 
 
                     </div>
                     <div className='col-2'></div>
-                   
+
                 </div>
 
 
